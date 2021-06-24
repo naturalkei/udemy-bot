16856f35cd36d987d08820a543e9dee9aaff3b8b
Natural Kei <natural@devpiece.com>
2021-06-25 04:37:24 +0900

    update start.sh

--- start.sh
+++ start.sh
@@ -2,4 +2,5 @@ if [ ! -d "./bin" ]; then
   exit 1
 fi
 source ./bin/activate
-python ./udemy_enroller.py --max-pages=2
+python ./udemy_enroller.py --max-pages=2 >> ~/logs/udemy-bot/work.log
+logrotate -f ./logging.conf
